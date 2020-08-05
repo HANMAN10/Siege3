1cde2052483cacd48c792239ac591f8e22757f0c
HANMAN10 <67202403+HANMAN10@users.noreply.github.com>
2020-08-05 10:57:06 -0700

    Add files via upload

---
+++ box.js
@@ -0,0 +1,48 @@
+class Box {
+
+    constructor(x, y, width, height, angle) {
+        var base_options = {
+            isStatic: false,
+            'restitution': 1,
+            'friction': 1,
+            'density': 1
+        }
+
+        this.body = Bodies.rectangle(x, y, width, height, base_options);
+        this.width = width;
+        this.height = height;
+        this.visiblity = 255;
+
+        World.add(world, this.body);
+    }
+
+    display() {
+
+        if(this.body.speed < 3) {
+            var pos = this.body.position;
+            var angle = this.body.angle;
+            push();
+            translate(this.body.position.x, this.body.position.y);
+            rotate(angle);
+            rectMode(CENTER);
+            fill("white");
+            rect(0, 0, this.width, this.height)
+            pop();
+          }
+          else{
+            World.remove(world, this.body);
+            push();
+            this.Visiblity -= 5;
+            tint(255, this.Visiblity);
+            pop();
+          }
+    }
+
+    score() {
+        if(this.Visiblity < 0 && this.Visiblity > -1005) {
+          score ++;
+          console.log(score);
+        }
+      }
+
+}
